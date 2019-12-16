9966c8c37c87bf7b5de467c5f86a3c7cd820ff22
OurDemocracy <48531575+OurDemocracy@users.noreply.github.com>
2019-12-16 12:13:32 +0100

    Add files via upload

---
+++ Almacen.sol
@@ -0,0 +1,172 @@
+
+
+//CREANDO LA FABRICA DE ZOMBIES
+
+pragma solidity ^0.4.25;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
+
+
+//JAVA + WEB3 + URL CURL
+
+//// Así es como accederiamos a nuestro contrato:
+var abi = /* abi generado por el compilador */
+var ZombieFactoryContract = web3.eth.contract(abi)
+var contractAddress = /* nuestra dirección del contrato en Ethereum después de implementarlo */
+var ZombieFactory = ZombieFactoryContract.at(contractAddress)
+// `ZombieFactory` ha accedido a las funciones y eventos públicos de nuestro contrato
+
+// algunos eventos están escuchando para recoger el valor del texto:
+$("#ourButton").click(function(e) {
+  var name = $("#nameInput").val()
+  // Llama a la función `createRandomZombie` de nuestro contrato:
+  ZombieFactory.createRandomZombie(name)
+})
+
+// Escucha al evento de `NewZombie`, y actualiza la interfaz
+var event = ZombieFactory.NewZombie(function(error, result) {
+  if (error) return
+  generateZombie(result.zombieId, result.name, result.dna)
+})
+
+// Recogemos el adn del zombi y actualizamos nuestra imagen
+function generateZombie(id, name, dna) {
+  let dnaStr = String(dna)
+  // rellenamos el ADN con ceros si es menor de 16 caracteres
+  while (dnaStr.length < 16)
+    dnaStr = "0" + dnaStr
+
+  let zombieDetails = {
+    // los primeros 2 dígitos hacen la cabeza. Tenemos 7 posibles cabezas, entonces hacemos % 7
+    // para obtener un número entre 0 - 6, después le sumamos 1 para hacerlo entre 1 - 7. Tenemos 7
+    // imagenes llamadas desde "head1.png" hasta "head7.png" que cargamos en base a 
+    // este número:
+    headChoice: dnaStr.substring(0, 2) % 7 + 1,
+    // Los siguientes 2 dígitos se refieren a los ojos, 11 variaciones:
+    eyeChoice: dnaStr.substring(2, 4) % 11 + 1,
+    // 6 variaciones de camisetas:
+    shirtChoice: dnaStr.substring(4, 6) % 6 + 1,
+    // los últimos 6 digitos controlas el color. Actualiza el filtro CSS: hue-rotate
+    // que tiene 360 grados:
+    skinColorChoice: parseInt(dnaStr.substring(6, 8) / 100 * 360),
+    eyeColorChoice: parseInt(dnaStr.substring(8, 10) / 100 * 360),
+    clothesColorChoice: parseInt(dnaStr.substring(10, 12) / 100 * 360),
+    zombieName: name,
+    zombieDescription: "A Level 1 CryptoZombie",
+  }
+  return zombieDetails
+}
+
+// LOS ZOMBIES ATACAN INTERACTUAN
+
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
+
+//Cuando tienes multiples archivos y quieres importar uno dentro de otro, Solidity usa la palabra clave import:
+//AHORA VAMOS A TRABAJAR EN UN NUEVO DOCUMENTO QUE IMPORTARÁ LOS ANTERIORES
+
+
+
+pragma solidity ^0.4.19;
+
+import "./zombiefactory.sol";
+
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
+  }
+
+  // define la función aquí
+
+}
